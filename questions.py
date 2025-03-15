0583c0e7ba1c2d1c56b7b0b107fa8842dcc8aa8f
Santiago Ferreyra <santiferreyra528@gmail.com>
2025-03-15 01:16:03 -0300

    Realizando un sistema de puntuacion al juego

--- questions.py
+++ questions.py
@@ -22,6 +22,8 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+#El usuario cuenta con un sistema de puntos
+puntos = 0
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
@@ -38,7 +40,10 @@ for _ in range(3):
             # Se verifica si la respuesta es correcta
             if user_answer == correct_answers_index[question_index]:
                 print("¡Correcto!")
+                puntos += 1
                 break
+            else:
+                puntos = puntos - 0.5
         else:
             print('respuesta no valida')
             sys.exit(1)
@@ -48,4 +53,4 @@ for _ in range(3):
         print("Incorrecto. La respuesta correcta es:")
         print(answers[question_index][correct_answers_index[question_index]])
 # Se imprime un blanco al final de la pregunta
-print()
+print(f'Tus puntos son: {puntos}')
